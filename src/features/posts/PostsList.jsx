dc5e8c32efd2c47e00d76ee2259d8852acbe9413
JhonatanP16 <jhonatanpanduro16@gmail.com>
2023-01-28 14:29:34 -0500

    terminado

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
-import { selectAllPost } from './postsSlice';
-import { useSelector} from 'react-redux'
-import PostsAuthor from './PostsAuthor';
-import TimeAgo from './TimeAgo';
-import ReactionsButtons from './ReactionsButtons';
+import { selectAllPost,getPostsStatus,getPostsError,fetchPosts } from './postsSlice';
+import { useSelector,useDispatch} from 'react-redux'
+import { useEffect } from 'react';
+import PostsExcerpt from './PostsExcerpt';
+
 
 const PostsList = () => {
     /* const posts = useSelector(state => state.posts); */
+    const dispatch = useDispatch();
     const posts = useSelector(selectAllPost);
-
-    const orderPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
-    const renderedPosts = orderPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0,100)}</p>
-            <p className='postCredit'>
-                <PostsAuthor userId={post.userId}/>
-                <TimeAgo timestamp={post.date}/>
-            </p>
-            <ReactionsButtons post={post}/>
-        </article>
-    ))
+    const postStatus = useSelector(getPostsStatus);
+    const error = useSelector(getPostsError);
     
-  return (
-    <section>
-        <h2>Posts</h2>
-        {renderedPosts}
-    </section>
-  )
+    useEffect(() => {
+        if (postStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postStatus, dispatch])
+
+    let content;
+    if (postStatus === 'loading') {
+        content = <p>"Loading..."</p>;
+    } else if (postStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+    } else if (postStatus === 'failed') {
+        content = <p>{error}</p>;
+    }
+
+    return (
+        <section>
+            <h2>Posts</h2>
+            {content}
+        </section>
+    )
 }
 
 export default PostsList
